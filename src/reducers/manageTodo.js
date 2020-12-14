8d07235814ad0e308ac5ce33f0b9ee3f3ed38ab9
see-bet-code <brittani.elan.taylor@gmail.com>
2020-12-14 18:15:47 -0500

    Done.

--- src/reducers/manageTodo.js
+++ src/reducers/manageTodo.js
@@ -2,7 +2,14 @@ export default function manageTodo(state = {
   todos: [],
 }, action) {
 
-  console.log("reducer received this action:", action);
+  switch (action.type) {
+    case 'ADD_TODO':
+      const newTodos = state.todos.concat(action.payload.text)
+      console.log({ todos: newTodos });
+      return { todos: newTodos}
 
-  return state;
+    default: 
+      return state
+  }
+
 }
